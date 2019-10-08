c42446ef4f2eb04544267d7ab95ca74bcae6c360
Aurélien Vandel <aure.vandel@gmail.com>
2019-10-08 12:06:22 +0200

    feat: rename files after processing

--- src/main/scala/SftpPoll.scala
+++ src/main/scala/SftpPoll.scala
@@ -1,33 +1,19 @@
 import java.net.InetAddress
-import scala.concurrent.duration.Duration
-import scala.concurrent.{ Await, ExecutionContext, Future }
+import scala.concurrent.duration._
 
+import akka.Done
 import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import akka.stream.alpakka.ftp.scaladsl.Sftp
 import akka.stream.alpakka.ftp.{ FtpCredentials, SftpIdentity, SftpSettings }
 import akka.stream.scaladsl.{ Sink, Source }
-import akka.stream.{ ActorMaterializer, Materializer }
 import akka.util.ByteString
 import com.zengularity.benji.s3.{ S3, WSS3 }
-import play.api.libs.ws.BodyWritable
 import play.api.libs.ws.DefaultBodyWritables._
 import play.api.libs.ws.ahc.StandaloneAhcWSClient
 
 
 object SftpPoll extends App {
-
-  def putToS3[A: BodyWritable](storage: WSS3, bucketName: String, objName: String, data: => Source[A, _])(implicit m: Materializer): Future[Unit] = {
-    implicit def ec: ExecutionContext = m.executionContext
-
-    for {
-      bucketRef <- { // get-or-create
-        val ref = storage.bucket(bucketName)
-        ref.create(failsIfExists = false).map(_ => ref)
-      }
-      storageObj = bucketRef.obj(objName)
-      _ <- data runWith storageObj.put[A]
-    } yield ()
-  }
 
   implicit val system = ActorSystem("actor-system-sftp")
   implicit val materializer = ActorMaterializer()
@@ -48,20 +34,25 @@ object SftpPoll extends App {
   implicit val ws: StandaloneAhcWSClient = StandaloneAhcWSClient()
   val s3: WSS3 = S3("minio", "minio123", "http", "192.168.64.2:31984")
 
-  val res = Sftp
-    .ls("/incoming", settings)
-    .filter(ftpFile => ftpFile.isFile)
-    .mapAsyncUnordered(parallelism = 2) { ftpFile =>
+  Source.tick(0.seconds, 10.seconds, Done).runForeach { _ =>
+    Sftp
+      .ls("/incoming", settings)
+      .filter(ftpFile => ftpFile.isFile && !ftpFile.name.endsWith(".transferred"))
+      .mapAsyncUnordered(parallelism = 2) { ftpFile =>
 
-      val data: Source[ByteString, _] = Sftp.fromPath(ftpFile.path, settings)
-      putToS3[ByteString](s3, "my-bucket", ftpFile.name, data)
-        .map { _ =>
-          ftpFile.path
-        }
-    }
-    .runWith(Sink.seq)
+        val data: Source[ByteString, _] = Sftp.fromPath(ftpFile.path, settings)
 
-  val seq = Await.result(res, Duration.Inf)
-  seq.foreach(println)
-
+        for {
+          bucketRef <- { // get-or-create
+            val ref = s3.bucket("poc-bp2s")
+            ref.create(failsIfExists = false).map(_ => ref)
+          }
+          storageObj = bucketRef.obj(ftpFile.name)
+          _ <- data
+            .alsoTo(storageObj.put[ByteString])
+            .runWith(Sftp.move(file => file.path + ".transferred", settings).contramap(_ => ftpFile))
+        } yield ()
+      }
+      .runWith(Sink.ignore)
+  }
 }
