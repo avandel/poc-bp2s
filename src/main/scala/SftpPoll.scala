e9b41004bf37ce619b1bdd7de7e895e0b9efbc49
Aurélien Vandel <aure.vandel@gmail.com>
2019-10-08 15:29:54 +0200

    feat: pass args to program

--- src/main/scala/SftpPoll.scala
+++ src/main/scala/SftpPoll.scala
@@ -15,28 +15,42 @@ import play.api.libs.ws.ahc.StandaloneAhcWSClient
 
 object SftpPoll extends App {
 
+  val sftpUser = args(0)
+  val sftpPassword = args(1)
+  val sftpIp = args(2)
+  val sftpPort = args(3).toInt
+  val sftpPath = args(4)
+  val bucketName = args(5)
+  val s3User = args(6)
+  val s3Password = args(7)
+  val s3Host = args(8)
+  val intervalPoll = args(9).toInt
+
+  args.foreach(println)
+
   implicit val system = ActorSystem("actor-system-sftp")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
 
-  val identity = SftpIdentity.createFileSftpIdentity("/Users/aurelienvandel/.ssh/keys/ssh_host_rsa_key")
-  val credentials = FtpCredentials.create("aurelienvandel", "password")
-  val address = InetAddress.getByName("192.168.64.2")
+  //val identity = SftpIdentity.createFileSftpIdentity("/Users/aurelienvandel/.ssh/keys/ssh_host_rsa_key")
+  val credentials = FtpCredentials.create(sftpUser, sftpPassword)
+  val address = InetAddress.getByName(sftpIp)
 
   val settings = SftpSettings
     .create(address)
-    .withPort(31436)
+    .withPort(sftpPort)
     .withCredentials(credentials)
     //.withSftpIdentity(identity)
     .withStrictHostKeyChecking(false)
 
   implicit val ws: StandaloneAhcWSClient = StandaloneAhcWSClient()
-  val s3: WSS3 = S3("minio", "minio123", "http", "192.168.64.2:31984")
+  val s3: WSS3 = S3(s3User, s3Password, "http", s3Host)
 
-  Source.tick(0.seconds, 10.seconds, Done).runForeach { _ =>
+
+  Source.tick(0.seconds, intervalPoll.seconds, Done).runForeach { _ =>
     Sftp
-      .ls("/incoming", settings)
+      .ls(sftpPath, settings)
       .filter(ftpFile => ftpFile.isFile && !ftpFile.name.endsWith(".transferred"))
       .mapAsyncUnordered(parallelism = 2) { ftpFile =>
 
@@ -44,7 +58,7 @@ object SftpPoll extends App {
 
         for {
           bucketRef <- { // get-or-create
-            val ref = s3.bucket("poc-bp2s")
+            val ref = s3.bucket(bucketName)
             ref.create(failsIfExists = false).map(_ => ref)
           }
           storageObj = bucketRef.obj(ftpFile.name)
