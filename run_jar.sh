42eb9b61abcd7d02855d68472c8002e92516df71
Stéphane TANKOUA <s.tankouat@gmail.com>
2019-10-15 15:34:34 +0200

    updating env arg

--- run_jar.sh
+++ run_jar.sh
@@ -1,19 +1,20 @@
 #! /bin/sh
 
 interval="$SYNC_INTERVAL"
-port="$SFTP_PORT"
-ip="$SFTP_ADDR"
+port="$BP2S_SFTP_SVC_SERVICE_PORT"
+ip="$BP2S_SFTP_SVC_SERVICE_HOST"
 user="$SFTP_USER"
 password="$SFTP_PASSWORD"
+sftpKey="$SFTP_PK"
 path="$SFTP_PATH"
-baseurl="$S3_BASE_URL"
+baseurl="$BP2S_STORAGE_SVC_SERVICE_HOST:$BP2S_STORAGE_SVC_SERVICE_PORT"
 accesskeyid="$S3_ACCESS_KEY_ID"
 secretkey="$S3_SECRET_KEY"
 targetbucket="$S3_BUCKET"
+kafkaBootstrapServers="$BP2S_KAFKA_SVC_SERVICE_HOST:$BP2S_KAFKA_SVC_SERVICE_PORT"
 kafkaTopic="$KAFKA_TOPIC"
+sftpPk="$SFTP_PK"
 
+echo java -jar sftp-poll-assembly-0.1.jar $user $password $ip $port $path $targetbucket $accesskeyid $secretkey $baseurl $interval $kafkaBootstrapServers $kafkaTopic $sftpPk
 
-echo $ip
-echo java -jar sftp-poll-assembly-0.1.jar $user $password $ip $port $path $targetbucket $accesskeyid $secretkey $baseurl $interval $kafkaTopic
-
-java -jar sftp-poll-assembly-0.1.jar $user $password $ip $port $path $targetbucket $accesskeyid $secretkey $baseurl $interval $kafkaTopic
+java -jar sftp-poll-assembly-0.1.jar $user $password $ip $port $path $targetbucket $accesskeyid $secretkey $baseurl $interval $kafkaBootstrapServers $kafkaTopic $sftpPk
--- src/main/scala/SftpPoll.scala
+++ src/main/scala/SftpPoll.scala
@@ -16,6 +16,7 @@ import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
 import play.api.libs.ws.DefaultBodyWritables._
 import play.api.libs.ws.ahc.StandaloneAhcWSClient
+import java.time.Instant
 
 
 object SftpPoll extends App {
@@ -30,33 +31,34 @@ object SftpPoll extends App {
   val s3Password = args(7)
   val s3Host = args(8)
   val intervalPoll = args(9).toInt
-  val kafkaTopic = args(10)
-
-  args.foreach(println)
+  val kafkaBootstrapServers = args(10)
+  val kafkaTopic = args(11)
+  val sftpKey =  new String(java.util.Base64.getDecoder.decode(args(12)))
 
   implicit val system = ActorSystem("actor-system-sftp")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
-
-  //val identity = SftpIdentity.createFileSftpIdentity("/Users/aurelienvandel/.ssh/keys/ssh_host_rsa_key")
   val credentials = FtpCredentials.create(sftpUser, sftpPassword)
   val address = InetAddress.getByName(sftpIp)
+
+  val identity = SftpIdentity.createRawSftpIdentity(sftpKey.getBytes())
 
   val settings = SftpSettings
     .create(address)
     .withPort(sftpPort)
     .withCredentials(credentials)
-    //.withSftpIdentity(identity)
+    .withSftpIdentity(identity)
     .withStrictHostKeyChecking(false)
 
   implicit val ws: StandaloneAhcWSClient = StandaloneAhcWSClient()
   val s3: WSS3 = S3(s3User, s3Password, "http", s3Host)
 
   val kafkaProducerSettings = ProducerSettings(system, new StringSerializer, new StringSerializer)
-    // .withBootstrapServers(bootstrapServers)
+    .withBootstrapServers(kafkaBootstrapServers)
 
   Source.tick(0.seconds, intervalPoll.seconds, Done).runForeach { _ =>
+    println(s"${Instant.now()} ---> start polling")
     Sftp
       .ls(sftpPath, settings)
       .filter(ftpFile => ftpFile.isFile && !ftpFile.name.endsWith(".transferred"))
@@ -73,7 +75,10 @@ object SftpPoll extends App {
             .alsoTo(storageObj.put[ByteString])
             .runWith(Sftp.move(file => file.path + ".transferred", settings).contramap(_ => ftpFile))
         } yield Message(storageObj.bucket, storageObj.name)
-      }.map(message => new ProducerRecord[String, String](kafkaTopic, message.jsonify))
+      }.map { message => 
+        println(s"${Instant.now()} ---> sending message $message to kafka topic $kafkaTopic")
+        new ProducerRecord[String, String](kafkaTopic, message.jsonify)
+      }
       .runWith(Producer.plainSink(kafkaProducerSettings))
   }
 }
